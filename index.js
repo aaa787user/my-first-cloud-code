d449ce7c421e0747156604a5647d4e9127016dcb
aaa787user <1772744203@qqq.com>
2026-04-28 13:43:48 +0800

    fix: 最终版部署配置

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-index.js
 export default {
-  async fetch(request, env, ctx) {
-    return new Response('哈喽！我的第一个Cloud Code部署成功啦！', {
-      headers: { 'Content-Type': 'text/html; charset=utf-8' },
+  async fetch(request) {
+    return new Response("Hello from Cloudflare Workers! Deployment successful 🎉", {
+      headers: { "Content-Type": "text/plain" },
     });
   },
 };
